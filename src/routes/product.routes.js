d5d5c3860976f48bf97852a4e2e55d063c3f6b2d
fredericgalline <frederic@galline.fr>
2023-03-17 15:02:16 +0100

    Implement all CRUD operations for products API
    - Add GET route to fetch all products
    - Add GET route to fetch a single product by ID
    - Add POST route to create a new product
    - Add PUT route to update a product by ID
    - Add DELETE route to remove a product by ID

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,24 +1,93 @@
-const connection = require('../db-config');
-const router = require('express').Router();
+const connection = require("../db-config");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-
+// GET all products
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM product", (err, results) => {
+    if (err) {
+      res.status(500).send("Error retrieving products from database");
+    } else {
+      res.json(results);
+    }
+  });
 });
 
-router.get('/:id', (req, res) => {
-
+// GET product by ID
+router.get("/:id", (req, res) => {
+  const productId = req.params.id;
+  connection.query(
+    "SELECT * FROM product WHERE id = ?",
+    [productId],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error retrieving product from database");
+      } else {
+        if (results.length === 0) {
+          res.status(404).send("Product not found");
+        } else {
+          res.json(results[0]);
+        }
+      }
+    }
+  );
 });
 
-router.post('/', (req, res) => {
-
+// POST a new product
+router.post("/", (req, res) => {
+  const { name, price, description, picture } = req.body;
+  connection.query(
+    "INSERT INTO product (name, price, description, picture) VALUES (?, ?, ?, ?)",
+    [name, price, description, picture],
+    (err, result) => {
+      if (err) {
+        res.status(500).send("Error saving product to database");
+      } else {
+        res.status(201).json({ id: result.insertId, ...req.body });
+      }
+    }
+  );
 });
 
-router.put('/:id', (req, res) => {
+// PUT (update) a product by ID
+router.put("/:id", (req, res) => {
+  const productId = req.params.id;
+  const updatedProduct = req.body;
 
+  connection.query(
+    "UPDATE product SET ? WHERE id = ?",
+    [updatedProduct, productId],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error updating product in database");
+      } else {
+        if (results.affectedRows === 0) {
+          res.status(404).send("Product not found");
+        } else {
+          res.status(200).json({ id: parseInt(productId), ...updatedProduct });
+        }
+      }
+    }
+  );
 });
 
-router.delete('/:id', (req, res) => {
-
+// DELETE a product by ID
+router.delete("/:id", (req, res) => {
+  const productId = req.params.id;
+  connection.query(
+    "DELETE FROM product WHERE id = ?",
+    [productId],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error deleting product from database");
+      } else {
+        if (results.affectedRows === 0) {
+          res.status(404).send("Product not found");
+        } else {
+          res.status(200).send(`Product with ID ${productId} was deleted`);
+        }
+      }
+    }
+  );
 });
 
 module.exports = router;
